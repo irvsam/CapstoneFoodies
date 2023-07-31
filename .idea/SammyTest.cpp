e704b3bec171d4edec71ef5e1695e59d57bcfa82
irvsam <126156693+irvsam@users.noreply.github.com>
2023-07-31 13:39:29 +0200

    first commit test

---
+++ .idea/SammyTest.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sammy on 2023/07/31.
+//
+
+#include "SammyTest.h"
---
+++ .idea/SammyTest.h
@@ -0,0 +1,16 @@
+//
+// Created by sammy on 2023/07/31.
+//
+
+#ifndef CAPSTONEFOODIES_SAMMYTEST_H
+#define CAPSTONEFOODIES_SAMMYTEST_H
+
+
+
+class SammyTest {
+
+};
+
+
+
+#endif //CAPSTONEFOODIES_SAMMYTEST_H
