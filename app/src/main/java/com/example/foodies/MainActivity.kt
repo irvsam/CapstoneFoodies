4c12bccae52b3ef82cdd3ceb15796dc886b1398f
jimmysleep <james.lankester@gmail.com>
2023-08-12 15:38:23 +0200

    WORKING: - Home Page and View VendorList button - VendorView populated

--- app/src/main/java/com/example/foodies/MainActivity.kt
+++ app/src/main/java/com/example/foodies/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.foodies
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val viewVendorsButton = findViewById<Button>(R.id.button_ViewVendors)
+        viewVendorsButton.setOnClickListener {
+            val intent = Intent(this, VendorListActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/foodies/VendorListActivity.kt
@@ -0,0 +1,15 @@
+package com.example.foodies
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class VendorListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vendor_list)
+
+    }
+
+}
