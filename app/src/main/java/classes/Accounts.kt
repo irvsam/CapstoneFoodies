fb77ce5a6497ce99951cb2ecfbbc641df8aac8b1
irvsam <irvsam001@myuct.ac.za>
2023-08-13 16:42:42 +0200

    First proper commit of the new classes

--- app/src/main/java/classes/Accounts.kt
+++ app/src/main/java/classes/Accounts.kt
@@ -1,6 +1,8 @@
+package classes
+
 //this one holds all classes relating to accounts
 
-class Account(
+open class Account(
     var userName: String,
     var password: String,
     var email: String,
@@ -8,8 +10,8 @@ class Account(
     var loggedIn: Boolean,
 )
 
-class UserAccount:Account()
+class UserAccount
 
-class VendorAccount:Account()
+class VendorAccount
 
 class RewardSystem(val point:Int, val conversion: Double)
