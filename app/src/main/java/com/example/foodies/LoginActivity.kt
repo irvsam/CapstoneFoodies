f2466f47e22a0124c7b8b1993a2fbf891dcdf31a
irvsam <irvsam001@myuct.ac.za>
2023-08-14 20:34:39 +0200

    ACTIVITIES: added register link to login page

--- app/src/main/java/com/example/foodies/LoginActivity.kt
+++ app/src/main/java/com/example/foodies/LoginActivity.kt
@@ -1,6 +1,13 @@
 package com.example.foodies
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
@@ -8,5 +15,20 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val registerLinkTextView = findViewById<TextView>(R.id.registerLink)
+
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                // Handle the click event, navigate to the registration screen
+
+            }
+        }
+
+        // Set the ClickableSpan to the TextView
+        val spannableString = SpannableString(registerLinkTextView.text)
+        spannableString.setSpan(clickableSpan, 23, 40, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        registerLinkTextView.text = spannableString
+        registerLinkTextView.movementMethod = LinkMovementMethod.getInstance()
     }
 }
