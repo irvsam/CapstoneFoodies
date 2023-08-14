3da92d0b835ed53cc9c4573e1947a17e9ee14a8c
irvsam <irvsam001@myuct.ac.za>
2023-08-14 21:07:02 +0200

    ACTIVITIES: got rid of home fragment and just have vendor list fragment as first page

--- app/src/main/java/com/example/foodies/MainActivity.kt
+++ app/src/main/java/com/example/foodies/MainActivity.kt
@@ -1,43 +1,19 @@
 package com.example.foodies
 
-import HomeFragment
 import VendorListFragment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
-
-    private val homeFragment = HomeFragment()
-    private lateinit var sharedViewModel: SharedViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        sharedViewModel = ViewModelProvider(this)[SharedViewModel::class.java]
-        sharedViewModel.onViewVendorsAction.observe(this) {
-
-            // must create a new transaction object every time you need one
+        if (savedInstanceState == null) {
             val transaction = supportFragmentManager.beginTransaction()
             transaction.replace(R.id.main_container, VendorListFragment())
-            transaction.addToBackStack(null)
             transaction.commit()
-
         }
-
-        if (savedInstanceState == null) {
-
-                val transaction = supportFragmentManager.beginTransaction()
-                // A 'transaction' allows you to add, replace, and remove fragments
-                // here we are replacing the fragment
-                transaction.replace(R.id.main_container, homeFragment)
-                    // this enables the user to use the back button to return to the previous page
-                    .addToBackStack(null)
-                    // commit() applies the changes
-                    .commit()
-        }
-
     }
 }
