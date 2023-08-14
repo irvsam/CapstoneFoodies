3ddec7a34a0be66705023e86a391f6c1e0115035
irvsam <irvsam001@myuct.ac.za>
2023-08-14 17:08:33 +0200

    added files without merge errors

---
+++ app/src/main/java/com/example/foodies/AccountActivity.kt
@@ -0,0 +1,12 @@
+package com.example.foodies
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AccountActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account)
+    }
+}
