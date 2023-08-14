bb9e5350441c7b708d5f1efd6e49847159c78d20
irvsam <irvsam001@myuct.ac.za>
2023-08-14 21:51:19 +0200

    WORKING! all buttons working

--- app/src/main/java/com/example/foodies/AccountActivity.kt
+++ app/src/main/java/com/example/foodies/AccountActivity.kt
@@ -1,6 +1,8 @@
 package com.example.foodies
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class AccountActivity: AppCompatActivity() {
@@ -8,5 +10,17 @@ class AccountActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account)
+
+
+
+        // Set click listener for the logout button
+
+        val signOutButton = findViewById<Button>(R.id.sign_out)
+        signOutButton.setOnClickListener {
+            // Navigate to MainActivity
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            // Optional: finish the LoginActivity if you don't want to come back to it using the back button
+        }
     }
 }
