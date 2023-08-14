93378b1d27e907cc452a6f1e518e0f7818d14590
irvsam <irvsam001@myuct.ac.za>
2023-08-14 09:16:11 +0200

    METHODS: fixed a bug

--- app/src/main/java/classes/Actions.kt
+++ app/src/main/java/classes/Actions.kt
@@ -2,9 +2,9 @@ package classes//all app actions
 
 class Notification(val message: String){
     fun notify(account: Account){}
-    fun notifyAll(){}
+    fun notifyAllAccounts(){}
 }
 
-class Filter()
+class Filter
 
-class Search()
+class Search
