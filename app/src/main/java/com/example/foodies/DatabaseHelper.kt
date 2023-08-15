eac3033c9ac9b9094666e6f3c3fbb97ae0db5370
jimmysleep <james.lankester@gmail.com>
2023-08-15 23:15:17 +0200

    Added SQL commands to create the tables in the appropriate structure

--- app/src/main/java/com/example/foodies/DatabaseHelper.kt
+++ app/src/main/java/com/example/foodies/DatabaseHelper.kt
@@ -13,19 +13,69 @@ class DatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME
 
     override fun onCreate(db: SQLiteDatabase) {
         // Create your tables here
-        val createVendorTable = """
-            CREATE TABLE vendor (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                name TEXT,
-                address TEXT,
-                phone TEXT
-            )
-        """.trimIndent()
-        db.execSQL(createVendorTable)
+        db.execSQL(CREATE_VENDOR_TABLE)
+        db.execSQL(CREATE_MENU_TABLE)
+        db.execSQL(CREATE_DIETARY_REQ_TABLE)
+        db.execSQL(CREATE_MENU_ITEM_TABLE)
+        db.execSQL(CREATE_REVIEW_TABLE)
     }
-
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
         // Handle database upgrades
         // You might want to preserve existing data during upgrades
     }
+
+    private val CREATE_VENDOR_TABLE = """
+        CREATE TABLE vendor (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            cuisine TEXT,
+            menu_id INTEGER,
+            rating REAL,
+            openTime TEXT,
+            closeTime TEXT,
+            dietaryReq_id INTEGER,
+            FOREIGN KEY(menu_id) REFERENCES menu(id),
+            FOREIGN KEY(dietaryReq_id) REFERENCES dietaryreq(id)
+        )
+    """.trimIndent()
+
+    private val CREATE_DIETARY_REQ_TABLE = """
+            CREATE TABLE dietary_req (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            type TEXT
+        )
+    """.trimIndent()
+
+    private val CREATE_MENU_TABLE = """
+        CREATE TABLE menu (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            vendor_id INTEGER,
+            name TEXT,
+            FOREIGN KEY(vendor_id) REFERENCES vendor(id)
+        )
+    """.trimIndent()
+
+    private val CREATE_MENU_ITEM_TABLE = """
+        CREATE TABLE menuitem (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            menu_id INTEGER,
+            name TEXT,
+            price REAL,
+            FOREIGN KEY(menu_id) REFERENCES menu(id)
+        )
+    """.trimIndent()
+
+
+    private val CREATE_REVIEW_TABLE = """
+        CREATE TABLE review (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            vendor_id INTEGER,
+            text TEXT,
+            rating REAL,
+            FOREIGN KEY(vendor_id) REFERENCES vendor(id)
+        )
+    """.trimIndent()
+    // Create other table creation strings (CREATE_MENU_TABLE, CREATE_DIETARY_REQ_TABLE, CREATE_REVIEW_TABLE)
 }
+
+
