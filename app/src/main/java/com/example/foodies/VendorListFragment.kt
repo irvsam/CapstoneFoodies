1fc92c414361d5f878fa7cbde9fe3173db280542
irvsam <irvsam001@myuct.ac.za>
2023-08-17 18:53:40 +0200

    attempting to convert to fragments

--- app/src/main/java/com/example/foodies/VendorListFragment.kt
+++ app/src/main/java/com/example/foodies/VendorListFragment.kt
@@ -1,44 +1,70 @@
 package com.example.foodies
-
 import android.content.Intent
-import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import classes.DietaryReq
+import classes.Menu
+import classes.Review
+import classes.Store
+import classes.StoreClickListener
+import classes.storeList
+import classes.storeRecyclerViewAdapter
+import com.example.foodies.AccountActivity
+import com.example.foodies.R
+import com.example.foodies.databinding.FragmentVendorListBinding
+import java.sql.Time
 
 class VendorListFragment : Fragment() {
 
-    private lateinit var dbHelper: DatabaseHelper
-    private lateinit var db: SQLiteDatabase
+    private lateinit var binding: FragmentVendorListBinding
+
+    private var imageList: List<Int> = listOf(R.drawable.cc, R.drawable.afriquezeen)
+    private var campusCafeMenu: Menu = Menu()
+    private var afriquezeenMenu: Menu = Menu()
+    private var ccReviewList: ArrayList<Review> = ArrayList()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_vendor_list, container, false)
+        binding = FragmentVendorListBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // create the databases
-        val dbHelper = DatabaseHelper(requireContext())
-        val db = dbHelper.writableDatabase // or readableDatabase
+        populateStores()
 
-        val backToHomeButton = view.findViewById<Button>(R.id.backToHome_button)
-        backToHomeButton.setOnClickListener{
-            val intent = Intent(requireContext(), LoginActivity::class.java)
-            startActivity(intent)
+        val mainActivity = requireActivity() as MainActivity
+
+        binding.storeRecyclerView.apply {
+            layoutManager = GridLayoutManager(requireContext(), 1)
+            adapter = storeRecyclerViewAdapter(storeList, mainActivity)
         }
 
-        val accButton = view.findViewById<Button>(R.id.account_button)
-        accButton.setOnClickListener{
+        val accountButton = binding.accountButton
+        accountButton.setOnClickListener {
             val intent = Intent(requireContext(), AccountActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun populateStores() {
+        val campusCafe: Store = Store(
+            "Campus Cafe", "Beverages", campusCafeMenu, 4.2,
+            Time(8, 15, 0), Time(16, 15, 0), DietaryReq.VEGETARIAN, ccReviewList, imageList[0]
+        )
+        val afriquezeen: Store = Store(
+            "Afriquezeen", "Hearty meals", afriquezeenMenu, 4.8,
+            Time(8, 15, 0), Time(16, 15, 0), DietaryReq.NUT_FREE, ccReviewList, imageList[1]
+        )
+        storeList.add(campusCafe)
+        storeList.add(afriquezeen)
     }
 }
