6da01bc6d507083947bc66badcbeca4abe66ec60
irvsam <irvsam001@myuct.ac.za>
2023-08-22 13:54:44 +0200

    added a browse fragment

---
+++ app/src/main/java/com/example/foodies/BrowseFragment.kt
@@ -0,0 +1,39 @@
+package com.example.foodies
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+// BrowseFragment.kt
+class BrowseFragment : Fragment() {
+
+    private lateinit var searchEditText: EditText
+    private lateinit var filterOption1: CheckBox
+    private lateinit var searchButton: Button
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_browse, container, false)
+        searchEditText = view.findViewById(R.id.searchEditText)
+        filterOption1 = view.findViewById(R.id.filterOption1)
+        searchButton = view.findViewById(R.id.searchButton)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        searchButton.setOnClickListener {
+            //perform the search
+            //check which filters are selected?
+        }
+    }
+}
