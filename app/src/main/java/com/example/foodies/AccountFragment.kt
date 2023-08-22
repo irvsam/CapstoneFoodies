0f620ee191bfe9e957e328cc6a77e11a52aff49b
irvsam <irvsam001@myuct.ac.za>
2023-08-22 11:15:04 +0200

    sign out button added

--- app/src/main/java/com/example/foodies/AccountFragment.kt
+++ app/src/main/java/com/example/foodies/AccountFragment.kt
@@ -1,9 +1,11 @@
 package com.example.foodies
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TableLayout
 import android.widget.TextView
@@ -20,6 +22,20 @@ class AccountFragment: Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_account, container, false)
 
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val signOutButton = view.findViewById<Button>(R.id.signout_button)
+
+        signOutButton.setOnClickListener {
+            // Perform the sign-out logic
+            //sharedViewModel.loggedIn = false
+
+            val intent = Intent(requireContext(), LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 
