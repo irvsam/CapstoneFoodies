9a378ef58b83f777ca73d0788dc8b1fb68c2e392
irvsam <irvsam001@myuct.ac.za>
2023-09-05 21:26:54 +0200

    everything seems good with account details and nav bar

--- app/src/main/java/com/example/foodies/StoreDetailsFragment.kt
+++ app/src/main/java/com/example/foodies/StoreDetailsFragment.kt
@@ -1,23 +1,30 @@
 package com.example.foodies
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import classes.GuestViewModel
 import classes.STORE_EXTRA
 import classes.Store
 import classes.storeList
 import com.example.foodies.databinding.FragmentStoreDetailsBinding
+import com.google.android.material.snackbar.Snackbar
 
 class StoreDetailsFragment : Fragment() {
 
     private lateinit var binding: FragmentStoreDetailsBinding
+    private lateinit var guestViewModel: GuestViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,10 +46,18 @@ class StoreDetailsFragment : Fragment() {
         }
         val actionBar: ActionBar? = (activity as AppCompatActivity).supportActionBar
         actionBar?.setDisplayHomeAsUpEnabled(true)
-
+        guestViewModel = ViewModelProvider(requireActivity())[GuestViewModel::class.java]
+        if(guestViewModel.isGuest){binding.reviewButton.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.grey))}
         binding.reviewButton.setOnClickListener {
-            val navController = findNavController()
-            navController.navigate(R.id.leaveReviewFragment)
+            if(!guestViewModel.isGuest){
+                    binding.reviewButton.setOnClickListener {
+                            // User is logged in, handle the review action here
+                            val navController = findNavController()
+                            navController.navigate(R.id.leaveReviewFragment)
+                    }
+            }
+            else{
+                Toast.makeText(requireContext(),"you are not logged in!",Toast.LENGTH_SHORT).show() }
         }
 
     }
@@ -69,4 +84,5 @@ class StoreDetailsFragment : Fragment() {
         }
         return super.onOptionsItemSelected(item)
     }
+
 }
