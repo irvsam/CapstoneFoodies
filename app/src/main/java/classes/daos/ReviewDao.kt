1058460c86404cc23f54334a71ddb312941b4495
jimmysleep <james.lankester@gmail.com>
2023-09-06 16:32:37 +0200

    moved ReviewDao to dao folder

--- app/src/main/java/classes/daos/ReviewDao.kt
+++ app/src/main/java/classes/daos/ReviewDao.kt
@@ -1,7 +1,25 @@
 package classes.daos
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import classes.Entities
 
 @Dao
 interface ReviewDao {
+    @Insert
+    suspend fun insertReview(review: Entities.Review)
+
+    @Update
+    suspend fun updateReview(review: Entities.Review)
+
+    @Delete
+    suspend fun deleteReview(review: Entities.Review)
+
+    @Query("SELECT * FROM review WHERE user_id = :userId")
+    suspend fun getReviewsByUserId(userId: Long): List<Entities.Review>
+
+    // Add other query methods as needed
 }
