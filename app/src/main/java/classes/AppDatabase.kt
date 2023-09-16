8170a25ae9a10a41b80ef15282e8d0b8e96a141b
irvsam <irvsam001@myuct.ac.za>
2023-09-16 13:45:05 +0200

    made grey

--- app/src/main/java/classes/AppDatabase.kt
+++ app/src/main/java/classes/AppDatabase.kt
@@ -16,7 +16,7 @@ import classes.daos.VendorDao
     Entities.MenuItem::class,
     Entities.Menu::class,
     Entities.Review::class],
-    version = 16, exportSchema = false
+    version = 17, exportSchema = false
 )
 
 abstract class AppDatabase : RoomDatabase() {
