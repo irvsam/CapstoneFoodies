46d5419b511aeef23ce5c868c4ebeecd50bcba53
wkrrya <wkrrya001@myuct.ac.za>
2023-09-18 16:53:14 +0200

    Search bar is working(may want to change search criteria), on click is not

--- app/src/main/java/com/example/foodies/BrowseFragment.kt
+++ app/src/main/java/com/example/foodies/BrowseFragment.kt
@@ -1,6 +1,8 @@
 package com.example.foodies
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,22 +11,40 @@ import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.ImageButton
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import classes.Entities
+import classes.SearchSharedViewModel
+import classes.SharedViewModel
+import classes.StoreClickListener
+import classes.StoreViewModel
+import classes.VendorManagementViewModel
+import classes.adapters.MenuItemAdapter
+import classes.adapters.SearchAdapter
 
 // BrowseFragment.kt
-class BrowseFragment : Fragment() {
+class BrowseFragment : Fragment(),StoreClickListener {
 
     private lateinit var searchEditText: EditText
     private lateinit var filterOption1: CheckBox
     private lateinit var searchButton: ImageButton
+    private lateinit var sharedViewModel: SharedViewModel
+    private lateinit var searchSharedViewModel: SearchSharedViewModel
+    private lateinit var vendorViewModel: StoreViewModel
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        sharedViewModel=ViewModelProvider(requireActivity())[SharedViewModel::class.java]
+        searchSharedViewModel = ViewModelProvider(requireActivity())[SearchSharedViewModel::class.java]
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_browse, container, false)
         searchEditText = view.findViewById(R.id.searchEditText)
-        filterOption1 = view.findViewById(R.id.filterOption1)
         searchButton = view.findViewById(R.id.searchButton)
         return view
     }
@@ -32,10 +52,61 @@ class BrowseFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val recyclerView: RecyclerView = view.findViewById(R.id.resultRecyclerView)
+        val itemAdapter = SearchAdapter(sharedViewModel.storeList,this,this)
+
+        searchEditText.addTextChangedListener(object : TextWatcher{
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val filteredSuggestions = filterSuggestions(s)
+                searchSharedViewModel.searchStoreList.observe(viewLifecycleOwner){ newStoreList ->
+                    newStoreList?.let {
+                        itemAdapter.storeList = filteredSuggestions
+                        itemAdapter.notifyDataSetChanged()
+                    }
+                }
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // No changes required
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // No changes required
+            }
+        })
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = itemAdapter
+
         searchButton.setOnClickListener {
             //perform the search
             //check which filters are selected?
 
         }
+    }
+
+    private fun filterSuggestions(word: CharSequence?):MutableList<Entities.Vendor?>{
+        val storeList = sharedViewModel.storeList
+        val filteredList = mutableListOf<Entities.Vendor?>()
+
+        for (store in storeList!!){
+            if (isCharSequenceInString(word!!,store!!.name)){
+                filteredList.add(store)
+                searchSharedViewModel.addStoreToSearch(store)
+            }
+        }
+        return filteredList
+    }
+
+    fun isCharSequenceInString(charSequence: CharSequence, inputString: String): Boolean {
+        val charSequenceLower = charSequence.toString().toLowerCase()
+        val inputStringLower = inputString.toLowerCase()
+
+        return inputStringLower.contains(charSequenceLower)
+    }
+
+    override fun onClick(store: Entities.Vendor?) {
+        vendorViewModel.vendor = store
+        // Open a new fragment when a store is clicked
+        val navController = findNavController()
+        navController.navigate(R.id.storeDetailsFragment)
     }
 }
