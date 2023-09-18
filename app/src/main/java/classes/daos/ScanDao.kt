20184beaa436fd673eacac848707a634a7295de6
jimmysleep <james.lankester@gmail.com>
2023-09-18 12:52:42 +0200

    Checkpoint before doing a big refactor to make StoreDetails more readable

--- app/src/main/java/classes/daos/ScanDao.kt
+++ app/src/main/java/classes/daos/ScanDao.kt
@@ -14,5 +14,8 @@ interface ScanDao {
     @Query("SELECT * FROM Scans")
     fun getAllScans(): LiveData<List<Entities.Scan>>
 
+    @Query("SELECT * FROM Scans WHERE vendorId = :vendorId")
+    fun getScansByVendor(vendorId: Long): List<Entities.Scan>
+
 
 }
