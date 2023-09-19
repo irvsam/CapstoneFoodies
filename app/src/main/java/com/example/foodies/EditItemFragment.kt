9986232cc8ec32dfeebe57d60fe45bec67bed75c
wkrrya <wkrrya001@myuct.ac.za>
2023-09-19 23:22:56 +0200

    Merging

--- app/src/main/java/com/example/foodies/EditItemFragment.kt
+++ app/src/main/java/com/example/foodies/EditItemFragment.kt
@@ -66,7 +66,7 @@ class EditItemFragment(private val menuItemID: Long) : DialogFragment() {
                                 orginalMenuItem,
                                 editedMenuItem
                             )
-
+                            dismiss()
                         }
                     }
                 }
